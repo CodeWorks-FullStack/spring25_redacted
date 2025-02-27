5f758e80062aac34247479b67203357d0467d229
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-27 13:10:37 -0700

    removed dummy data

--- app/AppState.js
+++ app/AppState.js
@@ -7,27 +7,7 @@ class ObservableAppState extends EventEmitter {
   /**
    * @type {CaseFile[]}
    */
-  caseFiles = [
-    // new CaseFile({
-    //   title: 'Moon Landing',
-    //   agency: 'A51',
-    //   content: 'We "landed" on the "moon" and everything was great.'
-    // }),
-    // new CaseFile({
-    //   title: 'Bigfoot',
-    //   agency: 'F&G',
-    //   content: 'Saw a big hairy creature in the woods. It was eating a hot dog.'
-    // }),
-    // new CaseFile({
-    //   title: 'Capybara Cryptid',
-    //   agency: 'IRS',
-    //   content: 'The Capybara Cryptid has been eating W2 forms for years and making our job much more difficult. We will audit him soon.'
-    // }),
-    // new CaseFile({
-    //   title: 'JFK',
-    //   agency: 'CIA'
-    // }),
-  ]
+  caseFiles = []
 
   /**
    * @type {CaseFile}
